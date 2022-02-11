bd468f0925078f9bc1a9842f9976d19455d7f96b
drakkar1969 <nicolabay@outlook.com>
2022-02-11 19:41:05 +0100

    Move widget initialization to end of preferences window constructor

--- app/zandronum-launcher.py
+++ app/zandronum-launcher.py
@@ -156,7 +156,6 @@ class PreferencesWindow(Adw.PreferencesWindow):
 
 		# Executable button
 		self.exec_btn = FileDialogButton(valign=Gtk.Align.CENTER, width_request=300, dlg_title="Select Zandronum Executable", dlg_parent=self, btn_icon="application-x-executable-symbolic")
-		self.exec_btn.set_selected_file(app.main_config["zandronum"]["exec_file"])
 
 		self.exec_listrow = Adw.ActionRow(title="Application _Path", use_underline=True)
 		self.exec_listrow.add_suffix(self.exec_btn)
@@ -164,7 +163,6 @@ class PreferencesWindow(Adw.PreferencesWindow):
 
 		# IWAD dir button
 		self.iwaddir_btn = FileDialogButton(valign=Gtk.Align.CENTER, width_request=300, dlg_title="Select IWAD Directory", dlg_parent=self, folder_select=True)
-		self.iwaddir_btn.set_selected_file(app.main_config["zandronum"]["iwad_dir"])
 
 		self.iwaddir_listrow = Adw.ActionRow(title="IWAD _Directory", use_underline=True)
 		self.iwaddir_listrow.add_suffix(self.iwaddir_btn)
@@ -172,7 +170,6 @@ class PreferencesWindow(Adw.PreferencesWindow):
 
 		# Mods switch
 		self.mods_switch = Gtk.Switch(valign=Gtk.Align.CENTER)
-		self.mods_switch.set_active(app.main_config["zandronum"]["use_mods"])
 
 		self.mods_listrow = Adw.ActionRow(title="Enable Hi-Res _Graphics", use_underline=True)
 		self.mods_listrow.add_suffix(self.mods_switch)
@@ -189,6 +186,13 @@ class PreferencesWindow(Adw.PreferencesWindow):
 		self.page.add(self.prefs_group)
 
 		self.add(self.page)
+
+		# Widget initialization
+		self.exec_btn.set_selected_file(app.main_config["zandronum"]["exec_file"])
+
+		self.iwaddir_btn.set_selected_file(app.main_config["zandronum"]["iwad_dir"])
+
+		self.mods_switch.set_active(app.main_config["zandronum"]["use_mods"])
 
 	def on_window_close(self, window):
 		app.main_config["zandronum"]["exec_file"] = self.exec_btn.get_selected_file()

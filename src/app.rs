1b3a78cc62e920a29bb71b30c6f76dc5ff03bf6e
drakkar1969 <nicolabay@outlook.com>
2023-10-07 12:06:53 +0200

    Bump version to 2.0.3

--- src/app.rs
+++ src/app.rs
@@ -110,7 +110,7 @@ impl ZLApplication {
             .application_name("Zandronum Launcher")
             .application_icon("zandronum")
             .developer_name("draKKar1969")
-            .version("2.0.2")
+            .version("2.0.3")
             .website("https://github.com/drakkar1969/zandronum-launcher/")
             .developers(vec!["draKKar1969"])
             .designers(vec!["draKKar1969"])
